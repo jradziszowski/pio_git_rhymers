bc336a082d85a927ea896bbe225ccf36ef42972f
Jan Radziszowski <255682@edu.p.lodz.pl>
2026-03-23 15:32:23 +0100

    11. Dokonanie hermetyzacji nieprywatnych atrybutów w klasach HanoiRhymer i FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,10 +1,9 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
-    @Override
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
